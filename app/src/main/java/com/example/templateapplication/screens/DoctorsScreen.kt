bb2f860f80b58963f814e35f72f5783daa94bfd3
Guippyyy <guillaume.decraene@student.hogent.be>
2023-11-05 15:26:38 +0100

    navigation

---
+++ app/src/main/java/com/example/templateapplication/screens/DoctorsScreen.kt
@@ -0,0 +1,26 @@
+package com.example.templateapplication.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.sp
+@Composable
+fun DoctorsScreen(){
+
+    Box(
+        modifier = Modifier
+                .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Doctors",
+            fontFamily = FontFamily.Serif,
+            fontSize = 22.sp
+        )
+    }
+
+}
