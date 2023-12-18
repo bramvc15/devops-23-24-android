8c4f6b0abab6e4b1803b2306e6bcaf4239c92b99
VictorHeyse <92031273+VictorHeyse@users.noreply.github.com>
2023-12-18 11:50:13 +0100

    Created RoomDatabase

---
+++ app/src/main/java/com/example/templateapplication/data/VisionDatabase.kt
@@ -0,0 +1,23 @@
+package com.example.templateapplication.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [dbDoctor::class], version = 1, exportSchema = false)
+abstract class VisionDatabase : RoomDatabase() {
+    abstract fun doctorDao(): DoctorDao
+    companion object {
+        @Volatile
+        private var Instance: VisionDatabase? = null
+        fun getDatabase(context: Context): VisionDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, VisionDatabase::class.java, "vision_database").fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+
+}
