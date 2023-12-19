0e70efd843941bc6bb827c86d02c9215358de4c8
Wout Vandenbogaerde <wout.vandenbogaerde@student.hogent.be>
2023-12-19 14:36:03 +0100

    styling

--- app/src/main/java/com/example/templateapplication/ui/screens/CalenderWeekScreen.kt
+++ app/src/main/java/com/example/templateapplication/ui/screens/CalenderWeekScreen.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -80,7 +82,7 @@ fun CalendarWeekScreen(doctorViewModel: DoctorViewModel, timeslotViewModel : Tim
         TopAppBar(
             elevation = 0.dp,
             title = {
-                Text(text = getWeekPageTitle(visibleWeek))
+                Text(text = getWeekPageTitle(visibleWeek), fontSize = 16.sp)
                 Box(modifier = Modifier
                         .fillMaxWidth()
                         .wrapContentHeight()
@@ -96,9 +98,9 @@ fun CalendarWeekScreen(doctorViewModel: DoctorViewModel, timeslotViewModel : Tim
                         expanded = dropdownExpanded,
                         onDismissRequest = { dropdownExpanded = false },
                         modifier = Modifier
-                                .width(200.dp)
                                 .background(Color.White)
                                 .align(Alignment.TopEnd)
+                                .fillMaxHeight()
                     ) {
                         doctorViewModel.doctors.collectAsState().value.forEach { doctor ->
                             DropdownMenuItem(
