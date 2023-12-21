353f4737a9c24c1c9ad376d374384798c5bdf2cb
Guippyyy <guillaume.decraene@student.hogent.be>
2023-12-21 12:33:40 +0100

    notes Screen + animations

--- app/src/main/java/com/example/templateapplication/MainActivity.kt
+++ app/src/main/java/com/example/templateapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
@@ -21,7 +20,7 @@ class MainActivity : ComponentActivity() {
             VisionApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                   // color = MaterialTheme.colorScheme.background
                 ) {
                     VisionApp()
                 }
--- app/src/main/java/com/example/templateapplication/ui/screens/notes/NoteScreen.kt
+++ app/src/main/java/com/example/templateapplication/ui/screens/notes/NoteScreen.kt
@@ -1,9 +1,12 @@
 package com.example.templateapplication.ui.screens.notes
 
 import android.annotation.SuppressLint
-import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,11 +16,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
-import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -29,22 +30,21 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.templateapplication.R
 import com.example.templateapplication.model.Note
 import com.example.templateapplication.ui.screens.notes.components.AddNoteDialog
 import com.example.templateapplication.ui.views.NoteViewModel
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun NoteScreen(
     noteViewModel : NoteViewModel = viewModel(factory = NoteViewModel.Factory),
@@ -52,78 +52,116 @@ fun NoteScreen(
 
     val notes by noteViewModel.notes.collectAsState()
     val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
     var isDialogOpen by remember { mutableStateOf(false) }
 
-
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    isDialogOpen = true
-                },
-                backgroundColor = MaterialTheme.colors.primary
-            ) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
-            }
-        },
-        scaffoldState = scaffoldState
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize(),
             horizontalAlignment = Alignment.Start
+
         )
         {
-        Text("YOUR NOTES", fontSize = 40.sp, modifier = Modifier.padding(10.dp) )
+            Text(
+                "Your notes",
+                fontSize = 40.sp,
+                modifier = Modifier.padding(10.dp),
+                color = MaterialTheme.colors.primary
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(16.dp))
 
-        LazyColumn(
-            modifier = Modifier.fillMaxSize()
-        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
 
 
-            items(notes) { note ->
-                NoteItem(note = note, onDeleteClicked = {
-                    // Handle note deletion here
-                    Log.d("note is here", "${note}")
-                    noteViewModel.deleteNote(note)
-                })
-                Spacer(modifier = Modifier.height(14.dp))
+                items(notes) { note ->
+//                    NoteItem(note = note, onDeleteClicked = {
+//                        noteViewModel.deleteNote(note)
+//                    })
+
+                    AnimatedNoteItem(
+                        note = note,
+                        onDeleteClicked = { noteViewModel.deleteNote(note) }
+                    )
+
+                    Spacer(modifier = Modifier.height(14.dp))
+                }
             }
         }
 
-        if (isDialogOpen) {
-            AddNoteDialog(
-                onDismiss = { isDialogOpen = false },
-                onNoteAdded = { title, content ->
-                    noteViewModel.insertNote(
-                        Note(
-                            title = title,
-                            content = content
-                        )
-                    )
-                    isDialogOpen = false
-                }
-            )
+        FloatingActionButton(
+            onClick = { isDialogOpen = true },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "Add note")
         }
+
+
+        AnimatedVisibility(
+            visible = isDialogOpen,
+            enter = fadeIn(),
+            exit =  fadeOut()
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(MaterialTheme.colors.primaryVariant)
+                    .fillMaxSize()
+                    .alpha(0.95f) // Adjust the alpha value as needed
+            ) {
+                AddNoteDialog(
+                    onDismiss = {
+                        isDialogOpen = false
+                    },
+                    onNoteAdded = { title, content ->
+                        noteViewModel.insertNote(
+                            Note(
+                                title = title,
+                                content = content
+                            )
+                        )
+
+                        isDialogOpen = false
+                    },
+                )
+            }
         }
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun AnimatedNoteItem(note : Note, onDeleteClicked: () -> Unit) {
+    AnimatedVisibility(
+        visible = true,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        NoteItem(note = note, onDeleteClicked = onDeleteClicked)
+    }
+}
+
+
+
 
 @Composable
-private fun NoteItem(note : Note, onDeleteClicked: () -> Unit){
+fun NoteItem(note : Note, onDeleteClicked: () -> Unit){
+
+    var isDeleting by remember { mutableStateOf(false) }
 
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .background(MaterialTheme.colors.surface)
-            .border(1.dp, MaterialTheme.colors.onSurface, shape = RectangleShape)
             .clip(MaterialTheme.shapes.medium),
-        elevation = 8.dp
+        elevation = 8.dp,
+        backgroundColor = colorResource(id = R.color.noteColorYellow)
     ) {
         Column(
             modifier = Modifier
@@ -135,13 +173,20 @@ private fun NoteItem(note : Note, onDeleteClicked: () -> Unit){
             Text(text = note.content, style = MaterialTheme.typography.body1)
         }
 
-        IconButton(
-            onClick = onDeleteClicked,
-
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.BottomEnd
         ) {
-            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete note")
+                IconButton(
+                    onClick = onDeleteClicked,
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete note"
+                    )
+                }
+            }
         }
     }
 
-}
 
